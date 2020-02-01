a8e1e7366f3212f668fdf47c578472dc84b748a6
Eduard <jimlab@mail.ru>
2020-02-01 23:26:06 +0300

    page login add form validations

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces';
 
 @Component({
   selector: 'app-login0-page',
@@ -30,5 +31,9 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
   }
 }
